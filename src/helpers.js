6690b89bb4e2f67987fc3cbcec0a47cda3d1d45a
Vilma <vilmail@protonmail.com>
2023-03-23 20:47:49 +0200

    cleaned rubbish from helpers

--- src/helpers.js
+++ src/helpers.js
@@ -3,34 +3,3 @@ export const rand = function (min, max) {
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
-
-const postTitleInputEl = document.querySelector("#add-post-form input[name='title']");
-const postContentInputEl = document.querySelector("#add-post-form input[name='content']");
-const postDescriptionInputEl = document.querySelector("#add-post-form input[name='description']");
-const postImageUrlInputEl = document.querySelector("#add-post-form input[name='img']");
-function fillFormWithPlaceholders() {
-  const postId = rand(1, 100);
-  const imgId = rand(1, 1000);
-  fetch("https://jsonplaceholder.typicode.com/posts/" + postId)
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-    })
-    .catch((e) => {
-      console.log(e);
-    });
-  fetch("https://jsonplaceholder.typicode.com/photos/" + imgId)
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-      console.log("filling with data");
-    })
-    .catch((e) => {
-      console.log(e);
-    });
-}
-function rand(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
